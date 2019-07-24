a8d83f3670ffd3aa4f91eff0b73718b4e01bd236
lakithaomal <lhw150030@utdallas.edu>
2019-07-24 20:36:46 +0000

    daily Updates

--- vapeNode/firmware/xu4/odroidShow2/display_image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat temp.raw > /dev/ttyUSB0
--- vapeNode/firmware/xu4/thermalMintsDisplay.sh
+++
@@ -1,7 +0,0 @@
-sleep 5 
-kill $(ps aux | grep '[p]ython3 thermalCamReader.py' | awk '{print $2}') &
-sleep 5 
-cd /home/teamlary/gitHubRepos/Lakitha/mobileAirNode/firmware/xu4 && python3 thermalCamReaderWithDisplay.py
-
-
-
